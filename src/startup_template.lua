9e4e47e79f53fd4192f88b9890570a5cb6aee04d
Lemon <107816109+feylostt@users.noreply.github.com>
2024-10-15 17:00:16 -0500

    added surface

--- src/startup_template.lua
+++ src/startup_template.lua
@@ -2,5 +2,5 @@ if fs.exists("./filename.lua") then
     fs.delete("./filename.lua")
 end
 
-shell.execute("wget", "url")
-shell.run("https://raw.githubusercontent.com/feylostt/lucky-lady/refs/heads/main/src/path")
+shell.execute("wget", "https://raw.githubusercontent.com/feylostt/lucky-lady/refs/heads/main/src/path")
+shell.run("filename")
